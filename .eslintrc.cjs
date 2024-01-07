8c40ba5d29380d34e679336deb0e8f0b6d4038cb
niklonskyi <niklonskyi@icloud.com>
2024-01-07 11:37:11 +0200

    build: Update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,7 @@
 module.exports = {
   extends: [
     'plugin:sonarjs/recommended',
-    'plugin:@typescript-eslint/recommended',
     'semistandard'
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   root: true
 };
